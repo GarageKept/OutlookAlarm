71495438c94ac0d8a8627559cbfae96d65496a1b
Darroll Walsh <darroll.walsh@microsoft.com>
2023-06-27 14:16:45 -0400

    Add new resources

--- src/GarageKept.OutlookAlarm/GarageKept.OutlookAlarm.Alarm/Properties/Resources.Designer.cs
+++ src/GarageKept.OutlookAlarm/GarageKept.OutlookAlarm.Alarm/Properties/Resources.Designer.cs
@@ -61,6 +61,36 @@ namespace GarageKept.OutlookAlarm.Alarm.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Byte[].
+        /// </summary>
+        internal static byte[] bell {
+            get {
+                object obj = ResourceManager.GetObject("bell", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Byte[].
+        /// </summary>
+        internal static byte[] bell_slash {
+            get {
+                object obj = ResourceManager.GetObject("bell_slash", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Byte[].
+        /// </summary>
+        internal static byte[] clock {
+            get {
+                object obj = ResourceManager.GetObject("clock", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.IO.UnmanagedMemoryStream similar to System.IO.MemoryStream.
         /// </summary>
         internal static System.IO.UnmanagedMemoryStream double_beep {
